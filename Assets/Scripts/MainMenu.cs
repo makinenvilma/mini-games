679c131cf6387541aa3464b06588cae67fe89662
makinenvilma <vilmakinen@outlook.com>
2025-04-29 12:32:50 +0300

    Connect the scenes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("PaintScene");
     }
 
     public void QuitGame()
